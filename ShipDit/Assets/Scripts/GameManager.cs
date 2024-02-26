15231c9c1da78e59cf7b84a2b286bc8a5f7ed2cf
AmirHossein M <amirgooli.maali@gmail.com>
2024-02-26 10:32:47 +0330

    working on camera

--- ShipDit/Assets/Scripts/GameManager.cs
+++ ShipDit/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -51,6 +52,10 @@ public class GameManager : MonoBehaviour
     public GameState gameState;
 
     public GameObject battleCamPos;
+
+    bool camIsMoving;
+
+    public GameObject placingCanvas;
 
     private void Awake()
     {
@@ -137,6 +142,7 @@ public class GameManager : MonoBehaviour
         players[activePlayer].placedShipList.Clear();
         InitGrid();
     }
+
     void InitGrid()
     {
         for (int x = 0; x < 10; x++)
@@ -157,6 +163,7 @@ public class GameManager : MonoBehaviour
                 players[activePlayer].placePanel.SetActive(false);
 
                 PlacingManager.Instance.SetPlayer(players[activePlayer].playfield, players[activePlayer].playerType.ToString());
+                StartCoroutine(MoveCamera(players[activePlayer].camPos));
                 gameState = GameState.IDLE;
                 break;
             case GameState.P2_PLACE_SHIPS:
@@ -203,11 +210,18 @@ public class GameManager : MonoBehaviour
         {
             HideAllMyShips();
             SwitchPlayer();
+            StartCoroutine(MoveCamera(players[activePlayer].camPos));
+            players[activePlayer].placePanel.SetActive(true);
+            return;
         }
         if (activePlayer == 1)
         {
             HideAllMyShips();
             SwitchPlayer();
+            StartCoroutine(MoveCamera(battleCamPos));
+            players[activePlayer].shootPanel.SetActive(true);
+            UnHideAllMyShips();
+            placingCanvas.SetActive(false);
         }
     }
 
@@ -231,5 +245,32 @@ public class GameManager : MonoBehaviour
     {
         activePlayer++;
         activePlayer %= 2;
+    }
+
+    IEnumerator MoveCamera(GameObject camObj)
+    {
+        if (camIsMoving)
+        {
+            yield break;
+        }
+        camIsMoving = true;
+
+        float t = 0;
+        float duration = 0.5f;
+
+        Vector3 startPos = Camera.main.transform.position;
+        Quaternion startRot = Camera.main.transform.rotation;
+
+        Vector3 endPos = camObj.transform.position;
+        Quaternion endPosRot = camObj.transform.rotation;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            Camera.main.transform.position = Vector3.Lerp(startPos, endPos, t / duration);
+            Camera.main.transform.rotation = Quaternion.Lerp(startRot, endPosRot, t / duration);
+            yield return null;
+        }
+        camIsMoving = false;
     }
 }
